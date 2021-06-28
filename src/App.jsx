8c2148547f1d4198b2a30812a8ef546adfcb9b24
MGanom <chewy111@naver.com>
2021-06-28 22:37:59 +0900

    add service, env

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,18 @@ import SearchHeader from './components/search_header/SearchHeader';
 import VideoList from './components/video_list/VideoList';
 import styles from './App.module.css';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=AIzaSyA1ZCTSgqHZeNmSwZfFjfEtWEyTgQrgh7M&part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyA1ZCTSgqHZeNmSwZfFjfEtWEyTgQrgh7M`,
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result =>
-        result.items.map(item => ({ ...item, id: item.id.videoId }))
-      )
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://www.googleapis.com/youtube/v3/videos?key=AIzaSyA1ZCTSgqHZeNmSwZfFjfEtWEyTgQrgh7M&part=snippet&chart=mostPopular&maxResults=25&regionCode=KR&key=AIzaSyA1ZCTSgqHZeNmSwZfFjfEtWEyTgQrgh7M',
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Youtube from './service/Youtube';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+ReactDOM.render(
+  <React.StrictMode>
+    <App youtube={youtube} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
