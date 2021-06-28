bec6d11cb7fc32412b32621637adda3eb2939c01
MGanom <chewy111@naver.com>
2021-06-28 23:44:06 +0900

    add: video iframe, detail 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from 'react';
 import SearchHeader from './components/search_header/SearchHeader';
 import VideoList from './components/video_list/VideoList';
 import styles from './App.module.css';
+import VideoDetail from './components/video_detail/VideoDetail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = video => {
+    setSelectedVideo(video);
+  };
+
   const search = query => {
     youtube
       .search(query) //
@@ -19,7 +26,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo ? (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        ) : null}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
     </div>
   );
 }
